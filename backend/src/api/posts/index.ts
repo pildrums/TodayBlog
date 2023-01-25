57778f56129b0fe28f8ddcc7358adde77c2f547f
pildrums <there7788@gmail.com>
2023-01-26 02:42:44 +0900

    Fixed post not working

--- backend/src/api/posts/index.ts
+++ backend/src/api/posts/index.ts
@@ -1,21 +1,13 @@
-import { Context } from "koa";
 import Router from "koa-router";
+import * as postsCtrl from "./posts.ctrl";
 
 const posts = new Router();
 
-const printInfo = (ctx: Context) => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params,
-  };
-};
-
-posts.get("/", printInfo);
-posts.post("/", printInfo);
-posts.get("/:id", printInfo);
-posts.delete("/:id", printInfo);
-posts.put("/:id", printInfo);
-posts.patch("/:id", printInfo);
+posts.get("/", postsCtrl.list);
+posts.post("/", postsCtrl.write);
+posts.get("/:id", postsCtrl.read);
+posts.delete("/:id", postsCtrl.remove);
+posts.put("/:id", postsCtrl.replace);
+posts.patch("/:id", postsCtrl.update);
 
 export default posts;
--- backend/src/api/posts/posts.ctrl.ts
+++ backend/src/api/posts/posts.ctrl.ts
@@ -12,8 +12,8 @@ const posts = [
 ];
 
 // Post 작성
-exports.write = (ctx: Context) => {
-  const { title, body } = ctx.Request.body;
+export const write = (ctx: any) => {
+  const { title, body } = ctx.request.body;
   postId += 1;
   const post = { id: postId, title, body };
   posts.push(post);
@@ -21,12 +21,12 @@ exports.write = (ctx: Context) => {
 };
 
 // Post 목록 조회
-exports.list = (ctx: Context) => {
+export const list = (ctx: any) => {
   ctx.body = posts;
 };
 
 // 특정 포스트 조회
-exports.read = (ctx: Context) => {
+export const read = (ctx: any) => {
   const { id } = ctx.params; // 문자열
   const post = posts.find((p) => p.id.toString() === id);
   if (!post) {
@@ -37,4 +37,58 @@ exports.read = (ctx: Context) => {
     return;
   }
   ctx.body = post;
+};
+
+// 포스트 삭제
+export const remove = (ctx: Context) => {
+  const { id } = ctx.params;
+  // 해당 id를 가진 포스트가 몇 번째인지 조회
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: "포스트가 없습니다.",
+    };
+    return;
+  }
+  posts.splice(index, 1);
+  ctx.status = 204;
+};
+
+// 포스트 수정(교체)
+export const replace = (ctx: any) => {
+  const { id } = ctx.params;
+  // 해당 id를 가진 포스트가 몇 번째인지 조회
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: "포스트가 없습니다.",
+    };
+    return;
+  }
+  posts[index] = {
+    id,
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
+
+// 포스트 수정(특정 필드 변경)
+export const update = (ctx: any) => {
+  const { id } = ctx.params;
+  // 해당 id를 가진 포스트가 몇 번째인지 조회
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: "포스트가 없습니다.",
+    };
+    return;
+  }
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
 };

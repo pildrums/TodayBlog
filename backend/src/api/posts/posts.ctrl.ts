85eda1bc03b737afb4903118341d7a404978eba8
pildrums <there7788@gmail.com>
2023-01-28 06:47:31 +0900

    Modify post.ctrl.ts about mongodb added

--- backend/src/api/posts/posts.ctrl.ts
+++ backend/src/api/posts/posts.ctrl.ts
@@ -1,95 +1,80 @@
-import { Context } from "koa";
+import Post from "../../models/post";
 
-let postId = 1; // id 초기값
-
-// posts 배열 초기 데이터
-const posts = [
+/*
+  POST api/posts
   {
-    id: 1,
-    title: "제목",
-    body: "내용",
-  },
-];
-
-// Post 작성
-export const write = (ctx: any) => {
-  const { title, body } = ctx.request.body;
-  postId += 1;
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
-};
-
-// Post 목록 조회
-export const list = (ctx: any) => {
-  ctx.body = posts;
-};
-
-// 특정 포스트 조회
-export const read = (ctx: any) => {
-  const { id } = ctx.params; // 문자열
-  // 해당 id를 가진 포스트 조회
-  const post = posts.find((p) => p.id.toString() === id);
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: "포스트가 없습니다.",
-    };
-    return;
+    title: '제목',
+    body: '내용',
+    tags: ['태그1', '태그2']
   }
-  ctx.body = post;
+  함수는 promise를 반환해야 하므로 함수는 async/await 문법 사용
+*/
+export const write = async (ctx: any) => {
+  const { title, body, tags } = ctx.request.body;
+  // 새로운 포스트 인스턴스 생성(new 구문 사용)
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  // await는 무조건 try/catch 문으로 오류처리 해야 함
+  try {
+    // 저장될 때까지 대기(await)
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
-// 포스트 삭제
-export const remove = (ctx: Context) => {
+export const list = async (ctx: any) => {
+  try {
+    // exec(): 서버에 쿼리 요청
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const read = async (ctx: any) => {
   const { id } = ctx.params;
-  // 해당 id를 가진 포스트가 몇 번째인지 조회
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: "포스트가 없습니다.",
-    };
-    return;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  posts.splice(index, 1);
-  ctx.status = 204;
 };
 
-// 포스트 수정(교체)
-export const replace = (ctx: any) => {
+export const remove = async (ctx: any) => {
   const { id } = ctx.params;
-  // 해당 id를 가진 포스트가 몇 번째인지 조회
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: "포스트가 없습니다.",
-    };
-    return;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
 };
 
-// 포스트 수정(특정 필드 변경)
-export const update = (ctx: any) => {
+export const update = async (ctx: any) => {
   const { id } = ctx.params;
-  // 해당 id를 가진 포스트가 몇 번째인지 조회
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: "포스트가 없습니다.",
-    };
-    return;
+  try {
+    // 첫번째 파라미터: id, 두번째 파라미터: 업데이트 내용, 세번째 파라미터: 업데이트 옵션
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터 반환
+      // false일 때는 업데이트되기 전의 데이터를 반환
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
 };

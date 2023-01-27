7492dc7b7bee4d86728f92f5453e24f5fa0d2ca3
pildrums <there7788@gmail.com>
2023-01-28 07:50:05 +0900

    Install bcrypt & Create user instance method

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,9 +1,21 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new Schema({
   username: String,
   hashedPassword: String,
 });
+
+// 화살표 함수를 사용할 경우 this 구문 사용 못함 (주의!)
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result;
+};
 
 const User = mongoose.model("User", UserSchema);
 
